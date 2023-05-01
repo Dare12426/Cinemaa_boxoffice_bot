560e50a4967c3cb448eb8ab166f87df5c408a3a8
Dare12426 <132150077+Dare12426@users.noreply.github.com>
2023-05-01 14:19:09 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -825,7 +825,7 @@ async def deletemultiplefiles(bot, message):
         parse_mode=enums.ParseMode.HTML
     )
 
-@Client.on_message(filters.command("shortlink") & filters.user(ADMINS))
+@Client.on_message(filters.command("shortlink"))
 async def shortlink(bot, message):
     chat_type = message.chat.type
     if chat_type == enums.ChatType.PRIVATE:
